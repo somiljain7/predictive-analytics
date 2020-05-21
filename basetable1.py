7810d08044c2783c860a332b1e9cab964990acc6
SOMIL JAIN <42209500+somiljain7@users.noreply.github.com>
2020-05-22 03:05:30 +0530

    snippet #1

---
+++ basetable1.py
@@ -0,0 +1,13 @@
+population_size  =len(basetable)
+
+# Print the population size.
+print(population_size)
+
+# Assign the number of targets to the variable 'targets_count'.
+targets_count = sum(basetable["target"])
+
+# Print the number of targets.
+print(targets_count)
+
+# Print the target incidence.
+print(targets_count/population_size)
